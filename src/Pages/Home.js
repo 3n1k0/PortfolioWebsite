ff25a813e4dbe23cdfad6ec378cb091c667b1aa0
Eniko Kosztolanyi <eniko.kosztolanyi@gmail.com>
2021-10-23 19:11:26 +0200

    clean up old elements

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,142 +1,38 @@
 import React from "react";
-import { device } from "../config/mediaquery";
-import styled from "styled-components/macro";
-import Typewriter from "typewriter-effect";
-import GlobalStyle from "../config/globalStyles";
-import Progress from "../components/Progress";
-import Navigation from "./Navigation";
-import About from "./Aboutme";
-import Portfolio from "./Portfolio";
-import Contact from "./Contact";
-import SectionContainer from "../components/SectionContainer";
-import Footer from "./Footer";
+import styled from "styled-components";
 
-
-const HomeContainer = styled.div`
-  width: 100%;
-  height: 100vh;
-  padding-top: 50px;
+const MainContainer = styled.div`
   display: flex;
-  flex-flow: column nowrap;
   align-items: center;
   justify-content: center;
-  overflow: hidden;
-  position: relative;
+  height: 100vh;
+  width: 100vw;
 `;
 
-const Hello = styled.div`
-  display: grid;
-  justify-items: center;
+const Contacts = styled.div`
+  width: 500px;
+  height: 500px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-  span {
-    display: none;
-
-    @media ${device.desktop} {
-      display: inline;
-    }
-  }
-
-  h2 {
-    font-weight: 400;
-    opacity: 1;
-    padding-bottom: 0;
-    margin: 0;
-  }
-
-  p {
-    padding-top: 0;
-    margin: 0;
-    padding-bottom: 30px;
-    color: rgba(0, 0, 0, 0.3);
-    font-size: 50px;
-  }
-
-  @media ${(device.desktop, device.tablet)} {
-    display: flex;
-    flex-flow: column;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 100vh;
-    text-align: center;
-    font-size: 50px;
-  }
-
-  i {
-    -moz-animation: bounce 1s infinite linear;
-    -o-animation: bounce 1s infinite linear;
-    -webkit-animation: bounce 1s infinite linear;
-    animation: bounce 1s infinite linear;
-    fill: papayawhip;
-    font-size: 38px;
-    position: relative;
-    padding-top: 100px;
+  li {
+    list-style-type: none;
   }
 `;
 
 const Home = () => {
-  console.log(window.scrollY);
-
   return (
-    <>
-      <GlobalStyle />
-
-      <SectionContainer>
-        <a href="/" name="home"> </a>
-        <Progress />
-        <HomeContainer>
-          <Hello>
-            <h2>Hello, I'm Enikő!</h2>
-            <p>[ ˈɛnikøː]</p>
-            <span>
-              <Typewriter
-                options={{
-                  autoStart: true,
-                  loop: true,
-                  deleteSpeed: 21,
-                  delay: 80,
-                }}
-                onInit={(typewriter) => {
-                  typewriter
-                    .start()
-                    .typeString("I'm a <b>FrontEnd Developer</b>")
-
-                    .pauseFor(1000)
-                    .deleteChars(18)
-                    .pauseFor(500)
-                    .typeString("<b>Problem Solver</b>")
-                    .pauseFor(500)
-                    .deleteChars(14)
-                    .pauseFor(500)
-                    .typeString("<b>Creative</b>")
-                    .pauseFor(500)
-                    .deleteChars(8)
-                    .pauseFor(500)
-                    .typeString("<b>Fast Learner</b>")
-                    .pauseFor(500);
-                }}
-              />
-            </span>
-            <i class="fas fa-angle-double-down"></i>
-          </Hello>
-        </HomeContainer>
-      </SectionContainer >
-
-      <SectionContainer>
-        <About />
-      </SectionContainer>
-
-      <SectionContainer>
-        <Portfolio />
-      </SectionContainer>
-
-      <SectionContainer>
-        <Contact />
-      </SectionContainer>
-
-      <Footer />
-      <Navigation />
-    </>
+    <MainContainer>
+      <Contacts>
+        <ul>
+          <li>Instagram</li>
+          <li>Github</li>
+          <li>Facebook</li>
+          <li>Photography</li>
+        </ul>
+      </Contacts>
+    </MainContainer>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Home from "./pages/Home";
-import About from "./pages/Aboutme";
-import Contact from "./pages/Contact";
-import Portfolio from "./pages/Portfolio";
 import Notfound from "./pages/Notfound";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 ReactDOM.render(
   <div>
     <BrowserRouter>
-      
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/about" exact component={About} />
-        <Route path="/contact" exact component={Contact} />
-        <Route path="/portfolio" exact compontent={Portfolio} />
         <Route compontent={Notfound} />
       </Switch>
     </BrowserRouter>
